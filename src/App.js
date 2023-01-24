93029e17e0020448bab8c1be88a52a2a29a4da71
Clay Hamilton <clayham@gmail.com>
2023-01-23 21:54:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Countdown, { calcTimeDelta } from "react-countdown";
+import { DateTime } from 'luxon';
+
+const meet = new Date("2023-02-08T21:00:00");
+
+const date = calcTimeDelta(meet)
+
+const meetingdate = DateTime.local(2023, 2, 8, 21, 0)
+
+const now = DateTime.now()
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Meg and Clay are Reunited Countdown :)</h1>
+        <h2 className='Box'>
+          <Countdown date={meet + Date.now()} />
+        </h2>
+        <img src="./IMG_4230.JPG" alt="megandclayhappy" width="300px" height="300px" className='Box'/>
+        <p>More to come...</p>
+        
       </header>
     </div>
+
   );
 }
 
